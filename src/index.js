ca45d4c9dc4bbb4b7a4db3aca151a14f4f79818d
jtkyber <43868043+jtkyber@users.noreply.github.com>
2021-03-17 14:30:12 -0600

    test

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import 'tachyons';
 import { HashRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
-  <Router basename={process.env.PUBLIC_URL}>
+  <Router>
     <App />
   </Router>,
   document.getElementById('root')
