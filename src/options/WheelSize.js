ca3bbaddb16863c3e25b3b94d138c86fa49b2d9f
jtkyber <43868043+jtkyber@users.noreply.github.com>
2021-03-21 12:19:14 -0600

    forgot to add previous changes to wheel size

--- src/options/WheelSize.js
+++ src/options/WheelSize.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import Options from '../components/Options';
 
-const WheelSize = ({ bikes, curMod }) => {
+const WheelSize = ({ bikes, bks, curMod }) => {
     const arr = [];
+    let classname = '';
     bikes.forEach(bike => {
         if ((curMod === '' || bike.model === curMod) && !arr.includes(bike.wheelSize)) {
             arr.push(bike.wheelSize);
@@ -11,8 +12,18 @@ const WheelSize = ({ bikes, curMod }) => {
 
     return (
         arr.sort().map(wS => {
+            for (let i = 0; i < bks.length; i ++) {
+                if (wS === bks[i].wheelSize) {
+                    classname = '';
+                    break;
+                }
+                else {
+                    classname = 'crossOut';
+                }
+            }
+
             let units = parseInt(wS) ? '"' : '';
-            return <Options option={wS} units={units} />
+            return <Options classname={classname} option={wS} units={units} />
         })
     )
 }
